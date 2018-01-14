7efcbd052578ad79f6d95cff030c7ea570fe2f4c
morfat <morfatmosoti@gmail.com>
2018-01-14 21:52:16 +0300

    set jquery

--- src/app/accounts/login/login.component.ts
+++ src/app/accounts/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+//import *  as $ from 'jquery';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,6 +12,7 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    //$('body').append("Workes jquery");
   }
 
 }
